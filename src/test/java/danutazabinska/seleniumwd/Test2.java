fdb42e1797b3c635971994492077a11022a0cc14
DanutaZabinska <danuta.irena.zabinska@gmail.com>
2017-04-02 12:29:18 +0200

    Test that checks the website title

---
+++ src/test/java/danutazabinska/seleniumwd/Test2.java
@@ -0,0 +1,52 @@
+package danutazabinska.seleniumwd;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class Test2 {
+
+	
+	static WebDriver driver;
+	
+	@BeforeClass
+	public static void setUp() {
+		
+		System.setProperty("webdriver.gecko.driver", "D:\\Danusia\\Testowanie\\geckodriver-v0.14.0-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+//		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void ShouldBeTitleLikeExpected() throws InterruptedException {
+		
+		String baseUrl = "http://linea-bianca.strefa.pl/";
+		String expectedTitle = "Linea Bianca - Hodowla Psów Rasowych FCI";
+		String actualTitle = " ";
+
+		driver.get(baseUrl);
+		
+		actualTitle = driver.getTitle();
+		if (actualTitle.contains(expectedTitle)) {
+			System.out.println("Test Passed");
+		} else {
+
+			System.out.println("Test Failed");
+		}
+		
+	
+		
+		
+	}	
+	
+	@AfterClass
+	public static void tearDown() {
+		driver.quit();
+//		System.exit(0);
+	}
+}
